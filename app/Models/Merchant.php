d5cc52bc81f17abd0e94e5d4e3c39c3e20dd5c93
Katarina Vucic <vucic.kat@gmail.com>
2022-04-24 13:35:17 +0200

    created model and migration for merchant

---
+++ app/Models/Merchant.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Merchant extends Model
+{
+    use HasFactory;
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'merchant_code';
+
+    /**
+     * The data type of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'display_name',
+        'address',
+        'phone_number',
+        'email',
+        'account_number',
+    ];
+}
---
+++ database/migrations/2022_04_24_112936_create_merchants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('merchants', function (Blueprint $table) {
+            $table->string('merchant_code')->unique();
+            $table->string('display_name');
+            $table->string('address');
+            $table->string('phone_number');
+            $table->string('email');
+            $table->string('account_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('merchants');
+    }
+};
